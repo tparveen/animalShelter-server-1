cad2d2300e0c2fd0d80e8badbd4b9daec6298382
Elias Mason <eliasjmason@gmail.com>
2017-10-25 01:54:12 -0700

    Implement dog and cat queues

--- index.js
+++ index.js
@@ -6,8 +6,59 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
+const Queue = require('./queue');
+
 const app = express();
 
+//function to create the queue of animals
+const dogQ = new Queue();
+const catQ = new Queue();
+
+function makeDogQueue(dog) {
+  //There is always a dog there
+  dogQ.enqueue({
+    //imageURL:'http:/', //add images to flick or AWS
+    imageURL:
+      'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg', //add images to flick or AWS
+    name: 'Zeus',
+    gender: 'Male',
+    age: '3 yrs',
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  });
+  dogQ.enqueue({
+    imageURL:
+      'http://www.dogbreedslist.info/uploads/allimg/dog-pictures/German-Shepherd-Dog-1.jpg',
+    name: 'Tornado',
+    gender: 'Female',
+    age: '5 yrs',
+    breed: 'German Shepard',
+    story: 'Owner moved to a small aparment'
+  });
+  dogQ.enqueue(dog);
+}
+function makeCatQueue(cat) {
+  catQ.enqueue({
+    imageURL:
+      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    name: 'Fluffy',
+    gender: 'Female',
+    age: '2 yrs',
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  });
+  catQ.enqueue({
+    imageURL:
+      'http://www.catvet.ca/wp-content/uploads/2016/07/cathealth_kitty.jpg',
+    name: 'Thunder',
+    gender: 'Male',
+    age: '1 yr',
+    breed: 'Taby',
+    story: 'Owner moved to another country'
+  });
+
+  catQ.enqueue(cat);
+}
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -21,7 +72,7 @@ app.use(
 );
 
 app.get('/api/cat', (req, res, next) => {
-  res.json({
+  makeCatQueue({
     imageURL:
       'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
     name: 'Fluffy',
@@ -30,18 +81,20 @@ app.get('/api/cat', (req, res, next) => {
     breed: 'Bengal',
     story: 'Thrown on the street'
   });
+  return res.json(catQ.dequeue());
 });
 
 app.get('/api/dog', (req, res, next) => {
-  res.json({
+  makeDogQueue({
     imageURL:
-      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+      'http://img.freepik.com/free-photo/husky-breed-dog-with-tongue-out_1187-1500.jpg?size=338&ext=jpg',
     name: 'June',
     gender: 'Female',
     age: '1 yrs',
     breed: 'Shiba',
     story: 'Rejected by mother'
   });
+  return res.json(dogQ.dequeue());
 });
 
 function runServer(port = PORT) {
