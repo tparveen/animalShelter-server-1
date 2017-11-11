a977a954f9c7d4a4b38a6a7ac0f6aae147920364
Elias Mason <eliasjmason@gmail.com>
2017-11-11 13:35:24 -0800

    refactor queueing

--- index.js
+++ index.js
@@ -12,36 +12,15 @@ const { Queue, peek } = require('./queue');
 
 const app = express();
 
-//function to create the queue of animals
+// Initialize empty queues for the cat and dog.
 const dogQ = new Queue();
 const catQ = new Queue();
 
-function makeDogQueue(dog) {
-  //There is always a dog there
-  dogQ.enqueue({
-    imageURL:
-      'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    description: 'A smiling golden-brown golden retreiver listening to music.',
-    name: 'Zeus',
-    gender: 'Male',
-    age: 3,
-    breed: 'Golden retriever',
-    story: 'Owner passed away.'
-  });
-  dogQ.enqueue({
-    imageURL:
-      'http://www.dogbreedslist.info/uploads/allimg/dog-pictures/German-Shepherd-Dog-1.jpg',
-    description: 'A German shepherd dog facing the camera, tongue out.',
-    name: 'Tornado',
-    gender: 'Female',
-    age: 5,
-    breed: 'German shepherd',
-    story: 'Owner moved to a small aparment.'
-  });
-  dogQ.enqueue(dog);
-}
-function makeCatQueue(cat) {
-  catQ.enqueue({
+// Functions for populating each queue.
+// Each queue will start with at least 2 default animals,
+// and also enqueue an optional third animal.
+function makeCatQueue(queue, newCat) {
+  queue.enqueue({
     imageURL:
       'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
     description: 'Orange bengal cat with black stripes lounging on concrete.',
@@ -51,7 +30,7 @@ function makeCatQueue(cat) {
     breed: 'Bengal',
     story: 'Thrown on the street.'
   });
-  catQ.enqueue({
+  queue.enqueue({
     imageURL:
       'http://www.catvet.ca/wp-content/uploads/2016/07/cathealth_kitty.jpg',
     description: 'Tan-colored kitten pawing at the camera.',
@@ -62,8 +41,38 @@ function makeCatQueue(cat) {
     story: 'Owner moved to another country.'
   });
 
-  catQ.enqueue(cat);
+  if (newCat) {
+    queue.enqueue(newCat);
+  }
 }
+
+function makeDogQueue(queue, newDog) {
+  queue.enqueue({
+    imageURL:
+      'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    description: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    gender: 'Male',
+    age: 3,
+    breed: 'Golden retriever',
+    story: 'Owner passed away.'
+  });
+  queue.enqueue({
+    imageURL:
+      'http://www.dogbreedslist.info/uploads/allimg/dog-pictures/German-Shepherd-Dog-1.jpg',
+    description: 'A German shepherd dog facing the camera, tongue out.',
+    name: 'Tornado',
+    gender: 'Female',
+    age: 5,
+    breed: 'German shepherd',
+    story: 'Owner moved to a small aparment.'
+  });
+  
+  if (newDog) {
+    queue.enqueue(newDog);
+  }
+}
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -81,7 +90,7 @@ app.use(
 // of the availability of the animals.
 
 app.get('/api/cat', (req, res, next) => {
-  makeCatQueue({
+  makeCatQueue(catQ, {
     imageURL: 'https://static.pexels.com/photos/20787/pexels-photo.jpg',
     description: 'Grey siamese cat with bright green eyes, looking up to the camera.',
     name: 'Tina',
@@ -94,7 +103,7 @@ app.get('/api/cat', (req, res, next) => {
 });
 
 app.get('/api/dog', (req, res, next) => {
-  makeDogQueue({
+  makeDogQueue(dogQ, {
     imageURL:
       'http://img.freepik.com/free-photo/husky-breed-dog-with-tongue-out_1187-1500.jpg?size=338&ext=jpg',
     name: 'June',
